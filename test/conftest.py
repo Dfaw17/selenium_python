a045d2e0acc8d6cf99ddb05d53fc9720df660491
DaffaPintu <daffa@pintu.co.id>
2024-05-28 20:02:41 +0700

    TEST

--- test/conftest.py
+++ test/conftest.py
@@ -8,9 +8,7 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def open_driver():
-    proxy_server_url = "117.54.114.98"
     options = Options()
-    options.add_argument(f'--proxy-server={proxy_server_url}')
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
